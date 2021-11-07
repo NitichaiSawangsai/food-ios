edcffe6152aed15a3248db7edb82644e84d937b0
NitichaiAom <NitichaiSawangsai@gmail.com>
2021-11-07 12:04:41 +0700

    first commit

---
+++ foodIOS/foodIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodIOSApp.swift
+//  foodIOS
+//
+//  Created by นิธิชัย แสวงสาย on 7/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct foodIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
